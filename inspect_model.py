4646733ac5fcbc7292ff3edd70860c657f27e3fb
Zalz <2210631170004@student.unsika.ac.id>
2026-03-12 15:30:51 +0700

    Remove deprecated files related to CCTV streaming and model inspection

--- inspect_model.py
+++
@@ -1,9 +0,0 @@
-# import the inference-sdk
-from inference_sdk import InferenceHTTPClient
-
-CLIENT = InferenceHTTPClient(
-    api_url="https://detect.roboflow.com",
-    api_key="59znzY448C1kIAKz9kyv"
-)
-
-result = CLIENT.infer(your_image.jpg, model_id="detek-apd/5")
